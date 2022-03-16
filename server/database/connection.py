6ed4e9e56a713c8fa5f1758e1693398f005669f6
nath <kendranatha108@gmail.com>
2022-03-16 13:10:27 +0500

    add models

--- server/database/connection.py
+++ server/database/connection.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from pathlib import Path
@@ -12,7 +11,7 @@ load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-engine = create_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession)
 
 engine.connect()
---
+++ server/database/models.py
@@ -0,0 +1,46 @@
+import asyncio
+from sqlalchemy import Integer, String, Column, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from connection import engine
+
+Base = declarative_base()
+
+class Countries(Base):
+    __tablename__ = 'countries'
+    id = Column(Integer, primary_key=True)
+    country_name = Column(String(255), nullable=False)
+
+class Currencies(Base):
+    __tablename__ = 'currencies'
+    id = Column(Integer, primary_key=True)
+    currency_name = Column(String(255), nullable=False)
+
+class PaymentType(Base):
+    __tablename__ = 'payment_type'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(255), nullable=False)
+
+class Requests(Base):
+    __tablename__ = 'requests'
+    id = Column(Integer, primary_key=True)
+    current_amount = Column(Integer, nullable=False)
+    current_country = Column(String(255), nullable=False)
+    current_currency = Column(String(255), nullable=False)
+    current_type = Column(String(255), nullable=False)
+    wanted_amount = Column(Integer, nullable=False)
+    wanted_country = Column(String(255), nullable=False)
+    wanted_currency = Column(String(255), nullable=False)
+    wanted_type = Column(String(255), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+
+
+async def async_create():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def async_drop():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+asyncio.run(async_create())
