1007749b8751fe89f97805ac0e19375a0d1d54e8
Ken-Dra <for.shit@yahoo.com>
2022-03-19 16:58:45 +0500

    add select and input elements

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,39 +1,98 @@
 <template>
   <div class="container">
-    <div class="col-sm-3">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Выберите страну</option>
-        <option value="1">Россия</option>
-        <option value="2">Турция</option>
-        <option value="3">Эстония</option>
-      </select>
+    <div class="row">
+      <label><b>Отдаете</b></label>
+      <div class="col-sm-3">
+        <select class="form-select" v-model="current_country">
+          <option disabled value="">Выберите страну</option>
+          <option value="1">Россия</option>
+          <option value="2">Турция</option>
+          <option value="3">Эстония</option>
+        </select>
+      </div>
+
+      <div class="col-sm-3">
+        <select class="form-select" v-model="current_type">
+          <option disabled value="">Выберите тип платежа</option>
+          <option value="1">Нал</option>
+          <option value="2">Банковский счет</option>
+        </select>
+      </div>
+
+      <div class="col-sm-3">
+        <select class="form-select" v-model="current_currency">
+          <option disabled value="">Выберите валюту</option>
+          <option value="1">RUB</option>
+          <option value="2">USD</option>
+          <option value="3">BTC</option>
+        </select>
+      </div>
+
+      <div class="col-sm-3">
+        <input
+          type="amount"
+          class="form-control"
+          placeholder="Введите сумму"
+          v-model="current_amount"
+        />
+      </div>
     </div>
 
-    <div class="col-sm-3">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Выберите тип платежа</option>
-        <option value="1">Нал</option>
-        <option value="2">Банковский счет</option>
-      </select>
-    </div>
+    <div class="row">
+      <label><b>Получаете</b></label>
+      <div class="col-sm-3">
+        <select class="form-select" v-model="wanted_country">
+          <option disabled value="">Выберите страну</option>
+          <option value="1">Россия</option>
+          <option value="2">Турция</option>
+          <option value="3">Эстония</option>
+        </select>
+      </div>
 
-    <div class="col-sm-3">
-      <select class="form-select" aria-label="Default select example">
-        <option selected>Выберите валюту</option>
-        <option value="1">RUB</option>
-        <option value="2">USD</option>
-        <option value="3">BTC</option>
-      </select>
-    </div>
+      <div class="col-sm-3">
+        <select class="form-select" v-model="wanted_type">
+          <option disabled value="">Выберите тип платежа</option>
+          <option value="1">Нал</option>
+          <option value="2">Банковский счет</option>
+        </select>
+      </div>
 
-    <div class="col-sm-3">
-      <input type="amount" class="form-control" placeholder="Введите сумму" />
+      <div class="col-sm-3">
+        <select class="form-select" v-model="wanted_currency">
+          <option disabled value="">Выберите валюту</option>
+          <option value="1">RUB</option>
+          <option value="2">USD</option>
+          <option value="3">BTC</option>
+        </select>
+      </div>
+
+      <div class="col-sm-3">
+        <input
+          type="amount"
+          class="form-control"
+          placeholder="Введите сумму"
+          v-model="wanted_amount"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      current_country: "",
+      current_type: "",
+      current_currency: "",
+      current_amount: "",
+      wanted_country: "",
+      wanted_type: "",
+      wanted_currency: "",
+      wanted_amount: "",
+    };
+  },
+};
 </script>
 
 <style>
