7f3a297619ef5a3edb6eae5a27564f53255d553f
Ken-Dra <for.shit@yahoo.com>
2022-06-22 16:14:04 +0530

    add vue auth components

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
   <b-navbar
     class="navbar"
     toggleable="lg"
@@ -8,12 +8,26 @@
       <b-nav-item @click="$router.push('/')">Создать заявку</b-nav-item>
       <b-nav-item @click="$router.push('/requestsList')">Активные заявки</b-nav-item>
       <b-nav-item @click="$router.push('/adminPanel')">Панель администратора</b-nav-item>
+      <div v-if="isLoggedIn">
+      <b-nav-item @click="logout">Выйти</b-nav-item>
+      </div>
     </b-navbar-nav>
   </b-navbar>
 </template>
 <script>
 export default {
   name: 'Navbar',
+  computed : {
+    isLoggedIn : function(){ return this.$store.getters.isLoggedIn}
+  },
+  methods: {
+    logout: function () {
+      this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login')
+        })
+    }
+  },
 }
 </script>
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,34 +1,87 @@
 import Vue from 'vue'
+import store from "../store"
 import Router from 'vue-router'
 import Request from "../components/request";
 import RequestsList from "../components/requestsList";
 import AdminPanel from "../components/adminPanel";
 import DealChat from "../components/chat";
 import ChatAdminView from "../components/chatAdminView";
+import Login from "../components/login";
+import Register from "../components/register";
+import Secure from "../components/secure";
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
+  mode: "history",
+
   routes: [
     {
       path: '/',
       component: Request,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/requestsList',
       component: RequestsList,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/adminPanel',
       component: AdminPanel,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/chat',
       component: DealChat,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/chatAdminView',
       component: ChatAdminView,
-    }
-  ]
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/secure',
+      name: 'secure',
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
+    },
+  ],
 });
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+export default router
