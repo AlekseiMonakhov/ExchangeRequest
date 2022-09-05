e306c5443bca3728b35203fd5fc31890524b7ed9
Ken-Dra <for.shit@yahoo.com>
2022-09-05 16:48:19 -0500

    add vuetify and view changes

--- client/src/components/requestsList.vue
+++ client/src/components/requestsList.vue
@@ -1,38 +1,98 @@
 <template>
-  <div class="root-element">
-    <div class="main-element" v-for="request in requests">
-      <b-card bg-variant="light" text-variant="black">
-        <b-card>
-          <div class="element-1">
-            <div class="element-2"><strong> {{ request.id }} </strong></div>
-            <div class="element-2" v-if="isCurrentUser(request.maker_username)"> Создатель заявки: Я</div>
-            <div class="element-2" v-else> Создатель заявки: {{ request.maker_username }} Рейтинг: {{ request.maker_rank }}</div>
-            <div class="element-2"> {{ request.created_on.replace(/T/, ' ').slice(0, -7) }}</div>
-          </div>
-          <div class="element-1">
-            <div class="element-2"><strong>Вы получите: </strong></div>
-            <div class="element-2"> {{ request.current_amount }} {{ request.current_currency }}</div>
-            <div class="element-2"> {{ request.current_type }}</div>
-            <div class="element-2"> {{ request.current_country }} {{ request.current_city }}</div>
-            <div class="element-2"> {{ request.current_bank }} {{ request.current_purpose }}</div>
-          </div>
-          <div class="element-1">
-            <div class="element-2"><strong> Вы отдаете: </strong></div>
-            <div class="element-2"> {{ request.wanted_amount }} {{ request.wanted_currency }}</div>
-            <div class="element-2"> {{ request.wanted_type }}</div>
-            <div class="element-2"> {{ request.wanted_country }} {{ request.wanted_city }}</div>
-            <div class="element-2"> {{ request.wanted_bank }} {{ request.wanted_purpose }}</div>
-          </div>
-          <div class="element-2"><strong> Профит: </strong> {{ request.profit }}</div>
-        </b-card>
-        <b-button v-if="isNoMaker(request.maker_id)" @click="send(request)" variant="primary">
-          Связаться
-        </b-button>
-        <b-button v-else-if="isLoggedIn" @click="deleteRequest(request)" variant="dark">Удалить заявку</b-button>
-        <b-button v-else @click="$router.push('/login')" variant="primary">Войти в аккаунт</b-button>
-      </b-card>
-    </div>
-  </div>
+  <v-container fluid>
+    <template>
+      <v-row>
+        <v-col
+          v-for="request in requests"
+          :key="request.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <v-card>
+            <v-list dense>
+              <v-list-item>
+                <v-list-item-content>
+                  <strong>{{ request.id }}</strong>
+                </v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.created_on.replace(/T/, ' ').slice(0, -7) }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content>Создатель заявки: </v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.maker_username }}
+                </v-list-item-content>
+
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content>Рейтинг</v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.maker_rank }}
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>ID</v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.maker_id }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-divider></v-divider>
+              <v-list-item>
+                <v-list-item-content>Вы отдаете:</v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.wanted_amount }} {{ request.wanted_currency }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content></v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.wanted_type }}
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
+              <v-list-item>
+                <v-list-item-content>Вы получите:</v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.current_amount }} {{ request.current_currency }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content></v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.current_type }}
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content>Профит</v-list-item-content>
+                <v-list-item-content class="align-end">
+                  {{ request.profit }}
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content></v-list-item-content>
+                <v-list-item-content>
+                  <b-button v-if="isNoMaker(request.maker_id)" @click="send(request)" variant="primary">
+                    Связаться
+                  </b-button>
+                  <b-button v-else-if="isLoggedIn" @click="deleteRequest(request)" variant="dark">Удалить заявку</b-button>
+                  <b-button v-else @click="$router.push('/login')" variant="primary">Войти в аккаунт</b-button>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </v-container>
 </template>
 
 <script>
@@ -45,6 +105,7 @@ export default {
   data() {
     return {
       requests: [],
+      itemsPerPage: 4,
     }
   },
   computed: {
@@ -163,12 +224,5 @@ export default {
 </script>
 
 <style scoped>
-.main-element {
-  display: flex;
-  flex-direction: column;
-  margin-left: 1%;
-  margin-top: 1%;
-  justify-content: center;
-}
 
 </style>
